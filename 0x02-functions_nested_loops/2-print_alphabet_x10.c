77e16d5f8d9397aadebabf1a8618730026ce37ee
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-10 09:04:43 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Prints the alphabet in lower case 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
