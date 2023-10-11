6e06c37b8151e9e8c893b58f6f9c6a6d00bcf78a
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-10 17:12:05 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,17 +6,27 @@ void times_table(void)
 {
 	int i = 0;
 
-	int j = 0;
+	int j;
 
 	int res = 0;
 
 	while (i < 10)
 	{
+		j = 0;
 		while (j < 10)
 		{
 			res = i * j;
-			j++;
+			_putchar((res < 10 ? '0' : '0' + res / 10));
+			_putchar('0' + res % 10);
+			if (j < 9)
+			{
+				_puthcar(',');
+				_putchar(' ');
+				_putchar(' ');
+				j++;
+			}
 		}
+		putchar('\n');
 		i++;
 	}
 }
