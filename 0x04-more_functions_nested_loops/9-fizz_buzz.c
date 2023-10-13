cf51afb1d06fa4f8306abdc01c3f5905e04844b0
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-12 20:32:18 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,14 +7,14 @@
  */
 void print_fizz_buzz(void)
 {
-	for (int i = 0; i < 100; i++)
+	for (int i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz ");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
 		else
 			printf("%d ", i);
 	}
