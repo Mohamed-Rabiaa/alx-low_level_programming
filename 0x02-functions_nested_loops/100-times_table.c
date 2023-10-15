2ca270f4be9df67e33d4c01eb55ea129530bdedb
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-15 00:57:45 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,10 +9,10 @@ void print_times_table(int n)
 	{
 		int i, j, res;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (j = 0; j <= n; j++)
+			for (j = 1; j <= n; j++)
 			{
 				res = i * j;
 				_putchar(',');
