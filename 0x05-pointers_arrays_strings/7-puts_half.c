0d8d1bd4753d6913ba31173d33431dab64a7362c
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-16 11:04:17 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,15 @@ void puts_half(char *str)
 	while (str[i] != '\0')
 		i++;
 
-	for (j = (i - 1) / 2; j < i; j++)
-		_putchar(str[j]);
+	if (i % 2 != 0)
+		j = (i - 1) / 2;
+	else
+		j = i / 2;
 
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
 	_putchar('\n');
 }
