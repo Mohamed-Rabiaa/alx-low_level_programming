b83481dbf6e29603a37f3c11566f8bfc2c65d704
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-18 12:12:32 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 
 	int n = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] == s2[i])
 			n = 0;
@@ -31,7 +31,9 @@ int _strcmp(char *s1, char *s2)
 		}
 		i++;
 	}
-	if (s1[i] == '\0' && s2[i] != '\0')
+	if (n == 0 && s1[i] == '\0' && s2[i] != '\0')
 		n = -15;
+	else if (n == 0 && s1[i] != '\0' && s2[i] == '\0')
+		n = 15
 	return (n);
 }
