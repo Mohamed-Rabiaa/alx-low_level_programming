92547ad011fe7dd7eb25911b6465f59a588bff06
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-18 04:32:24 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,15 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j = 0;
 
-	while (s1[i] != '\0')
+	while (dest[i] != '\0')
 		i++;
 
-	while (s2[j] != '\0')
+	while (src[j] != '\0')
 	{
-		s1[i] = s2[j];
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
-	s1[i] = '\0';
+	dest[i] = '\0';
+	return (dest);
 }
