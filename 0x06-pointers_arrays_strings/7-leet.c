f4283b581629c6a0dc4d119152c1c9d6eb713fec
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-19 10:37:21 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - encodes a string into 1337
+ * @s: the string to be encoded
+ *
+ * Return: the encoded string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char letters[] = "aeotl";
+
+	int numbers[] = {4, 3, 0, 7, 1};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == letters[j] || s[i] == (letters[j] - 32))
+				s[i] = numbers[j];
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
