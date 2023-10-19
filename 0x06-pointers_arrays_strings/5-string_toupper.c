4c04b492854702c932dca96e590eeda6d5977106
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-19 02:30:44 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @s: the string in lowercase
+ *
+ * Retrun: the string in uppercase
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] > 'Z' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
