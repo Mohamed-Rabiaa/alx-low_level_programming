0e2864554e0a851558f3b5845be76b17cafa5382
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-20 07:41:19 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,4 +4,26 @@
  */
 void print_number(int n)
 {
+	int i;
+
+	int r = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n >= 0)
+	{
+		if (n <= 9)
+		{
+			_putchar(n + '0');
+			n = r;
+		}
+		else
+		{
+			r = (n % 10) + r;
+			n = n / 10;
+		}
+	}
 }
