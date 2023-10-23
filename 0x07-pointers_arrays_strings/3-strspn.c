461067e1699e34ccbc4b94b3ee2deaf9a8365ee2
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-23 11:40:38 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,8 +17,8 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		found =0;
-		
+		found = 0;
+
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
