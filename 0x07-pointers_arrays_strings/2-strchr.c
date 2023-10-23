80f776ae9cc1d2855152ea796aae59eb5bab6268
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-23 08:17:04 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,22 +6,16 @@
 *Return:  a pointer to the first occurrence of the character c in the string s,
 *or NULL if the character is not found
 */
-char _tolower(char ch)
-{
-	if (ch >= 'A' && ch <= 'Z')
-		return (ch + 32);
-	return (ch);
-}
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	c = _tolower(c);
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (c == _tolower(s[i]))
+		if (c == s[i])
 			return (s + i);
 	}
+	if (c == '\0')
+		return (s + i);
 	return (0);
 }
