72d1d50ea2357f91b63c502cf1f4309982799292
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-25 13:26:43 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - prints a string in reverse
+ *@s: a string
+ */
+void _print_rev_recursion(char *s)
+{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
