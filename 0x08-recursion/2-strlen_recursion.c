5dce7c6d835c75d908fd55efa4a68dcb9318840d
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-25 18:02:03 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+/**
+ *_strlen_recursion - returns the length of a string
+ *@s: a string
+ *
+ *Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+}
