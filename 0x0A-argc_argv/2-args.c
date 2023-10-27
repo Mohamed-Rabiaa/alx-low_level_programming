90217607506cec6ee17896b2aece52dc468a36fd
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-10-27 15:40:30 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *@argc: the number of arguments passed to main
+ *@argv: an array that contains all the arguments of main
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
