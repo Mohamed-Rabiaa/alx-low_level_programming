ddb15560d576036ba2fd4b471002088f9cdada22
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-02 13:49:26 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,20 +9,38 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	/*unsigned int i;*/
+	unsigned int i;
 
-	void *ptr;
+	unsigned int *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
+	switch (size)
+	{
+	case sizeof(int):
+		ptr = (int *) malloc(nmemeb * sizeof(int));
+		break;
+
+	case sizeof(char):
+		ptr = (char *) malloc(nmemb * sizeof(char));
+		break;
+
+	case sizeof(double):
+		ptr = (double *) malloc(nmemb * sizeof(double));
+		break;
+	case sizeof(float):
+		ptr = (float) malloc(nmemb * sizeof(double));
+		break;
+	default:
+		break;
+	}
 
 	if (ptr == NULL)
 		return (NULL);
 
-	/*for (i = 0; i < nmemb; i++)
-	  ptr[i] = 0;*/
+	for (i = 0; i < nmemb; i++)
+	  ptr[i] = 0;
 
-	return ptr;
+	return (ptr);
 }
