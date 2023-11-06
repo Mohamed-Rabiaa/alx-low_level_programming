78299c868aa86a1ef2bfc6b5f52e77df0c15673f
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-06 11:16:25 -0800

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 0-OBJECT_LIKE_MACRO_H_
-#define 0-OBJECT_LIKE_MACRO_H_
+#ifndef OBJECT_LIKE_MACRO_H_
+#define OBJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
-#endif /*0-OBJECT_LIKE_MACRO_H_*\
+#endif
