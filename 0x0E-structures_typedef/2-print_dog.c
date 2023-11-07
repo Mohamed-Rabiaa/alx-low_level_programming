5a401f8d9fcb53ea352eaccdd35efe671185f5b4
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-06 16:51:53 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,14 +6,14 @@
  */
 void print_dog(struct dog *d)
 {
-	if(d != NULL)
+	if (d != NULL)
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 
-		if (d->age <= 0)
+		if (d->age < 0)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
