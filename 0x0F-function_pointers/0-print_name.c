cfe36bdec273d5ce87c674d694d15000c6e202ab
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-08 11:35:47 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ *print_name - prints a name
+ *@name: the name to print
+ *@f: a pointer to the function that determines how to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
