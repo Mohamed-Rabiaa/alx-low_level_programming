6f336a248e202caab599cbbffacd3c842ad8feb6
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-08 18:12:40 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,27 +15,29 @@ int main(int argc, char *argv[])
 
 	int (*ptr)(int, int);
 
-	if (argc >= 4)
+	if (argc < 4)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-
-		if (num2 == 0 && (strcmp(argv[2], "/") == 0
-				  || strcmp(argv[2], "%") == 0))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		ptr = get_op_func(argv[2]);
-		if (ptr == NULL)
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		res = ptr(num1, num2);
-		printf("%d\n", res);
+		printf("Error\n");
+		exit(98);
 	}
-	printf("Error\n");
-	exit(98);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (num2 == 0 && (strcmp(argv[2], "/") == 0
+			  || strcmp(argv[2], "%") == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	ptr = get_op_func(argv[2]);
+	if (ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	res = ptr(num1, num2);
+	printf("%d\n", res);
+
 	return (0);
 }
