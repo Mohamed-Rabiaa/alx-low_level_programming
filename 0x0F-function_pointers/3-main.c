fa1aeae89173009fea0d5609c9a697f4a7acb318
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-08 17:52:00 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 /**
  * main - check the code
@@ -19,25 +20,28 @@ int main(int argc, char *argv[])
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[3]);
 
-		if (num1 > 0 && num2 > 0)
+		if (num2 == 0 && (strcmp(argv[2], "/") == 0
+				  || strcmp(argv[2], "%") == 0))
 		{
-			ptr = get_op_func(argv[2]);
-			if (ptr == NULL)
-			{
-				printf("Error\n");
-				exit(99);
-			}
-			else
-			{
-				res = ptr(num1, num2);
-				printf("%d\n", res);
-			}
+			printrf("Error\n");
+			exit(100);
+		}
+		ptr = get_op_func(argv[2]);
+		if (ptr == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		else
+		{
+			res = ptr(num1, num2);
+			printf("%d\n", res);
 		}
 	}
 	else
 	{
 		printf("Error\n");
 		exit(98);
-       	}
+	}
 	return (0);
 }
