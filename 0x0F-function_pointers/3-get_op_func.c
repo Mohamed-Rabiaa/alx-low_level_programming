f76eae2b0b9b9cd17f7e723e1d4fe0fe6616e1f2
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-08 19:36:47 -0800

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,9 +22,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 6)
+	while (ops[i].op)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (*(ops[i].op) == *s)
 			return (ops[i].f);
 		i++;
 	}
