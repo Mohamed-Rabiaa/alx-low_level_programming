7a747c48e8c509ec9adbd40097bed520116425f8
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-16 06:52:14 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,12 +2,12 @@
 #include "lists.h"
 
 /**
- *len_list - returns the number of elements in a linked list_t list.
+ *list_len - returns the number of elements in a linked list_t list.
  *@h: the address of the first element in the list
  *
  *Return: the number of nodes in the list
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	const list_t *ptr = h;
 
