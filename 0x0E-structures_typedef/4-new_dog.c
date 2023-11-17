7057e1f467234aa0ee444ef3a041350c6c0c3d00
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-17 08:50:05 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,31 +13,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
 
-	if (d == 0)
+	if (d == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
-	if (name != 0)
-	{
-		d->name = strdup(name);
-		if (d->name == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-	}
-	d->age = age;
 
-	if (owner != 0)
+	d->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (d->name == NULL)
 	{
-		d->owner = strdup(owner);
-		if (d->owner == NULL)
-		{
-			free(d->name);
-			free(d);
-			return (NULL);
-		}
+		free(d);
+		return (NULL);
 	}
+
+	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	d->name = strcpy(d->name, name);
+	d->age = age;
+	d->owner = strcpy(d->owner, owner);
 
 	return (d);
 }
