f62581747abcfc24f9ca9141d180043de774f546
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-20 08:38:48 -0800

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head)
+	if (head)
 	{
-		temp = *head;
+		while (*head)
+		{
+			temp = *head;
 
-		*head = temp->next;
+			*head = temp->next;
 
-		free(temp);
-	}
+			free(temp);
+		}
 		head = NULL;
+	}
 }
