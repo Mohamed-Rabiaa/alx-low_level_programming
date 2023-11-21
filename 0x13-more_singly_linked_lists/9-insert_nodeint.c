d1e73f56e8054e28ced0f5f3eaa4555de2fdc668
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-21 01:51:26 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *temp, *new;
 
-	if (!head || !*head == NULL)
+	if (!head || !*head)
 		return (NULL);
 
 	temp = *head;
