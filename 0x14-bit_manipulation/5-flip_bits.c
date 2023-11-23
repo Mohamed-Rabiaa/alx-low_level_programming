98a601e696304bb1691dfeb29eb914807ae24f19
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-23 13:25:30 -0800

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,8 @@
 /**
  *flip_bits - returns the number of bits you would need to flip
  * to get from one number to another.
- *@n:
- *@m:
+ *@n: the first number
+ *@m: the second number
  *
  *Return: the number of bits you would need to flip
  */
