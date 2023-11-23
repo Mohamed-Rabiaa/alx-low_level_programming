81477a0e6e2684d7ac7a4a46bd9d9443911ee0cc
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-23 13:03:57 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(*n) * 8 - 1))
 		return (-1);
 
-	*n = *n & (1UL >> index);
+	*n = *n & ~(1UL << index);
 
 	return (1);
 }
