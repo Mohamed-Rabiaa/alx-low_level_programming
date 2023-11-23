7dc8de0a6449666cbc6b108e2dd90d889fc69363
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-23 13:21:29 -0800

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,11 @@
+/**
+ *flip_bits - returns the number of bits you would need to flip
+ * to get from one number to another.
+ *@n:
+ *@m:
+ *
+ *Return: the number of bits you would need to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+}
