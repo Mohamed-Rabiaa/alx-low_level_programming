478a7d9c9afcf3ea47f9334814ad1389a15ad95c
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-27 10:50:40 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,13 +19,6 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (*temp)
-	{
-		tc_length++;
-
-		temp++;
-	}
-
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
@@ -34,10 +27,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (*temp)
-		  {
-			  tc_length++;
-			  temp++;
-		  }
+		{
+			tc_length++;
+			temp++;
+		}
 		n = write(fd, text_content, tc_length);
 		if (n == -1 || n != tc_length)
 			return (-1);
