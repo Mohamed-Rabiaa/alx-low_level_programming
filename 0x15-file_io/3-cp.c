3cd7ea956de8eb288aa35d486ba8b2a28013b9aa
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-27 12:40:54 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,3 +1,13 @@
 #include <unistd.h>
 #include <fcntl.h>
-#include <
+/**
+ * main - check the code
+ *@argc: the number of arguments passed to main
+ *@argv: an array that contains all the arguments of main
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
