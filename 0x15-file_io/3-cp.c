bc63e82c567f43538b1050f22042317f589b2564
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-28 10:03:04 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
 /**
  * main - check the code
  *@argc: the number of arguments passed to main
@@ -10,10 +12,57 @@
  */
 int main(int argc, char *argv[])
 {
-	int count = argc;
+	int fd_read, fd_write, n_read, n_write, fd_read_close, fd_write_close;
 
-	char **arr = argv;
+	char *file_from, *file_to;
 
-	printf("%d %s", count, arr[1]);
+	char buffer[BUFF_SIZE];
+
+	if (argc != 3)
+	{
+		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	}
+	file_from = argv[1];
+
+	file_to = argv[2];
+
+	fd_read = open(file_from, O_RDONLY);
+	if (fd_read == -1)
+	{
+		exit(98);
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", file_from);
+	}
+
+	n_read = read(fd_read, buffer, BUFF_SIZE);
+	if (n_read == -1)
+	{
+		exit(98);
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", file_from);
+	}
+	fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 664);
+	n_write = dprintf(fd_write, "%s", buffer);
+	if (n_write == -1)
+	{
+		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	}
+
+	fd_read_close = close(fd_read);
+	if (fd_read_close == -1)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
+	}
+
+	fd_write_close = close(fd_write);
+	if (fd_write_close == -1)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
+	}
+
 	return (0);
 }
