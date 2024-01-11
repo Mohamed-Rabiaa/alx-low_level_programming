668ff3aa3463bdfb9689a13410b9db5d64d164ff
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-11 11:14:38 -0800

    Fixing insert_dnodeint func

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,8 +43,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == index && temp == NULL)
 	{
 		new->prev = temp;
-		if (temp)
-			temp->next = new;
+		if (new->prev)
+			new->prev->next = new;
 		return (new);
 	}
 	return (NULL);
