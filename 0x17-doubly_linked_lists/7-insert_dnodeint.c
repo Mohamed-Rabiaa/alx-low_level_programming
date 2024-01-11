7525a938214508b3642c7c7a506459c3541da8e6
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-11 09:49:22 -0800

    Fixing insert_dnodeint func

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,24 +4,20 @@
  *@h: a pointer to the pointer of the first node in the list
  *@idx: the index of the new node
  *@n: the integer of the new node
- *Returns: the address of the new node, or NULL if it failed
+ *Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *temp = *h;
-
+	dlistint_t *new, *temp;
 	unsigned int index = 0;
 
 	if (!h)
 		return (NULL);
-
+	temp = *h;
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-
 	if (idx == 0)
 	{
 		if (*h)
@@ -30,7 +26,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (new);
 	}
-
 	while (temp)
 	{
 		if (index == idx)
