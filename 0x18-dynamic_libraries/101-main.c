9269f2f0c9821136cb86e2d62dcd276b5bd5d65a
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-01-15 11:26:52 -0800

    Adding 101-make_me_win.sh script

---
+++ 0x18-dynamic_libraries/101-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d\n", 9);
+	printf("%d\n", 8);
+	printf("%d\n", 10);
+	printf("%d\n", 24);
+	printf("%d\n", 75);
+	printf("%d\n", 9);
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LD_PRELOAD=/root/alx-low_level_programming/0x18-dynamic_libraries/101-win_jackpot_lib.so
+./gm
