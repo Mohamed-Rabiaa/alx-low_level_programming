cbdc62b46a0d3e6c5df57571fe4c77623293242e
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-03-02 09:30:51 -0800

    a

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -11,9 +11,14 @@
 int main(void)
 {
     hash_table_t *ht;
-
+    hash_node_t hn;
+    long int index;
     ht = hash_table_create(1024);
     hash_table_set(ht, "betty", "cool");
     hash_table_set(ht, "hetairas ","mentioner");
+
+    index = key_index("betty", 1024);
+    hn = ht->array[index];
+    printf("%s\n", hn->value);
     return (EXIT_SUCCESS);
 }
