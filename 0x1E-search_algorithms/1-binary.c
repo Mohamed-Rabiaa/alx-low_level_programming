79cb3c6a7f4d362a13f5de729fa9227f7da76f71
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-05-06 10:55:41 -0700

    Adding binary_search func

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+int search(int *arr, int low, int high, int value);
+/**
+ *binary_search - Searches for a value in a sorted array of integers
+ *using the Binary search algorithm
+ *@array: a pointer to the first element of the array to search in
+ *@size: the number of elements in array
+ *@value: the value to search for
+ *
+ *Return: the index where value is located
+ *or -1 if value is not present in array or array is NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (!array)
+		return (-1);
+	return (search(array, 0, size - 1, value));
+}
+/**
+ *search - Searches for a value in a sorted array of integers
+ *using the Binary search algorithm and prints the searched array
+ *@arr: a pointer to the first element of the array to search in
+ *@low: the lowist index of the array
+ *@high: the highest index of the array
+ *@value: the value to search for
+ *
+ *Return: the index where value is located
+ *or -1 if value is not present in array
+ */
+int search(int *arr, int low, int high, int value)
+{
+	int  med, i = low;
+
+	if (high >= low)
+	{
+		printf("Searching in array: ");
+		while (i <= high)
+		{
+			if (i != high)
+				printf("%d, ", arr[i]);
+			else
+				printf("%d\n", arr[i]);
+			i++;
+		}
+		med = (low + high) / 2;
+		if (arr[med] == value)
+			return (med);
+		if (arr[med] < value)
+			return (search(arr, med + 1, high, value));
+		return (search(arr, low, med - 1, value));
+	}
+	return (-1);
+}
